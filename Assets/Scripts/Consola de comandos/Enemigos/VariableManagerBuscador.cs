521f9acffafb0bc3fbff2cc357b11aeff1ae9db8
FrancisPrz <denix2208@gmail.com>
2022-10-06 12:53:58 -0500

    Avance de Scriptable Object para consola de comandos

---
+++ Assets/Scripts/Consola de comandos/Enemigos/VariableManagerBuscador.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Data/VariablesBuscador")]
+public class VariableManagerBuscador : ScriptableObject
+{
+    public float playerDistance_SO;
+    public float awareAI_SO;
+    public float atkRange_SO;
+}
